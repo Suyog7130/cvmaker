e6130181d47cd00dfd2a82fe4d70a90c0b518519
Suyog Garg <gargsuyog@g.ecc.u-tokyo.ac.jp>
2026-02-23 02:05:16 +0800

    busytex.wasm: add CDN line

--- assets/engine.js
+++ assets/engine.js
@@ -23,7 +23,7 @@ export class LatexEngineManager {
     this.runner = null;
     this.engineName = "lualatex";
     this.engine = null;
-    this.root = "https://cdn.jsdelivr.net/gh/suyoggarg/cvmaker@main/core/busytex/"; // Default to CDN, but can be overridden by config
+    this.root = "https://cdn.jsdelivr.net/gh/suyog7130/cvmaker@main/core/busytex/"; // Default to CDN, but can be overridden by config
   }
 
   async initIfNeeded() {
